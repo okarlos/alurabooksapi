5efa197cb8b3b8846edc0d4c21f951258f82a3d7
Karlos Kliemann <karloskliemann@gmail.com>
2023-05-09 08:04:44 -0300

    starting use of map

--- app/main.js
+++ app/main.js
@@ -1,28 +1,11 @@
 let livros = []
 const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
-const elementoLivros = document.getElementById('livros')
 
 getBuscarLivros()
 
 async function getBuscarLivros() {
     const res = await fetch(endpointAPI)
     livros = await res.json()
-    console.table(livros)
+    let livrosComDesconto = aplicarDesconto(livros)
     exibirLivros(livros)
-}
-
-function exibirLivros (listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        elementoLivros.innerHTML += `
-        <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-            <h2 class="livro__titulo">${livro.titulo}</h2>
-            <p class="livro__descricao">${livro.autor}</p>
-            <p class="livro__preco" id="preco">${livro.preco}</p>
-            <div class="tags">
-                <span class="tag">${livro.categoria}</span>
-            </div>
-        </div>
-        `
-    });
 }
